1da0b025cc7496e351d04e4fe21a8a414cc0984e
kumar804 <kumarsinghmanish547@gmail.com>
2021-07-10 09:44:06 +0530

    calculating daily wage

--- Demo/HelloWorld.java
+++ Demo/HelloWorld.java
@@ -5,7 +5,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		int IS_FULL_TIME = 1;
-		
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int dailyWage = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
 			System.out.println("Employee is Present");
